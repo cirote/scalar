3a9b51f25bd3ac1fc5fd2bcca2c30da238a287e5
cirote <estebanrogel@gmail.com>
2019-10-27 23:41:09 -0300

    Correccion en la facade

--- src/Facade/Scalar.php
+++ src/Facade/Scalar.php
@@ -4,12 +4,6 @@ namespace Cirote\Scalar\Facade;
 
 use Illuminate\Support\Facades\Facade;
 
-/**
- * @method static mixed Density()
- *
- * @see \Illuminate\Cache\CacheManager
- * @see \Illuminate\Cache\Repository
- */
 class Scalar extends Facade
 {
     protected static function getFacadeAccessor()
